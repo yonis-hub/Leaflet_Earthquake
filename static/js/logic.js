91aaa1700961e40b1aec1fe2669f0e65a9958233
Yonis Hassan <hyonis@fb.com>
2021-05-24 09:45:11 -0500

    took out grayscale map

--- static/js/logic.js
+++ static/js/logic.js
@@ -90,18 +90,18 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  var grayscaleMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.light",
-    accessToken: API_KEY
-  });
+  // var grayscaleMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  //   maxZoom: 18,
+  //   id: "mapbox.light",
+  //   accessToken: API_KEY
+  // });
 
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Satellite": satelliteMap,
-    "GrayScale": grayscaleMap,
+    // "GrayScale": grayscaleMap,
     "Dark Map": darkmap,
     "Outdoors": outdoorsMap
   };
